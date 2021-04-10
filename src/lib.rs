5ec2bcff3c5bde018939daaea653891959cd023d
Elijah Woelbing <woelbingelijah@gmail.com>
2021-04-10 16:49:22 -0700

    passed tests

---
+++ src/lib.rs
@@ -0,0 +1,59 @@
+fn quicksort(arr: &mut Vec<i32>, low: i32, high: i32) {
+    if low < high {
+        let p = partition(arr, low, high);
+        quicksort(arr, low, p - 1); // before partition
+        quicksort(arr, p + 1, high) // after partition
+    }
+}
+
+fn partition(arr: &mut Vec<i32>, low: i32, high: i32) -> i32 {
+    let mut pivot = arr[high as usize]; // pivot
+    let mut i = low - 1; // right position of pivot
+    let mut j = low;
+    while j < high {
+        // if current is smaller than pivot
+        if arr[j as usize] < pivot {
+            i += 1;
+            swap(arr, i, j);
+        }
+        j += 1;
+    }
+    i += 1;
+    // put pivot in correct spot and new pivot is swaped in
+    swap(arr, i, high);
+    i
+}
+
+fn swap(arr: &mut Vec<i32>, pos_1: i32, pos_2: i32) {
+    let temp = arr[pos_1 as usize];
+    arr[pos_1 as usize] = arr[pos_2 as usize];
+    arr[pos_2 as usize] = temp;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn quicksort_test() {
+        let mut numbers = vec![4, 3, 7, 5, 6, 9, 1, 8];
+        let last = numbers.len() - 1;
+        quicksort(&mut numbers, 0, last as i32);
+        let sorted_numbers: Vec<i32> = vec![1, 3, 4, 5, 6, 7, 8, 9];
+        assert_eq!(sorted_numbers, numbers);
+    }
+    #[test]
+    fn swap_test() {
+        let mut numbers = vec![4, 3, 6];
+        swap(&mut numbers, 0, 2);
+        let swaped_numbers = vec![6, 3, 4];
+        assert_eq!(swaped_numbers, numbers);
+    }
+    #[test]
+    fn partition_test() {
+        let mut numbers = vec![4, 2, 6, 3, 8, 5];
+        let last = numbers.len() - 1;
+        partition(&mut numbers, 0, last as i32);
+        let partitioned_numbers = vec![4, 2, 3, 5, 8, 6];
+        assert_eq!(partitioned_numbers, numbers);
+    }
+}
