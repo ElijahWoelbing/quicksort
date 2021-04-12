0941601f09cf763ef314d01656e6eeed53cdf418
Elijah Woelbing <woelbingelijah@gmail.com>
2021-04-11 20:57:45 -0700

    passed tests for generic implmentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
-fn quicksort(arr: &mut Vec<i32>, low: i32, high: i32) {
+fn quicksort<T>(arr: &mut Vec<T>, low: i32, high: i32)
+where
+    T: PartialOrd + Copy,
+{
     if low < high {
         let p = partition(arr, low, high);
         quicksort(arr, low, p - 1); // before partition
@@ -6,7 +9,10 @@ fn quicksort(arr: &mut Vec<i32>, low: i32, high: i32) {
     }
 }
 
-fn partition(arr: &mut Vec<i32>, low: i32, high: i32) -> i32 {
+fn partition<T>(arr: &mut Vec<T>, low: i32, high: i32) -> i32
+where
+    T: PartialOrd + Copy,
+{
     let mut pivot = arr[high as usize]; // pivot
     let mut i = low - 1; // right position of pivot
     let mut j = low;
@@ -24,7 +30,11 @@ fn partition(arr: &mut Vec<i32>, low: i32, high: i32) -> i32 {
     i
 }
 
-fn swap(arr: &mut Vec<i32>, pos_1: i32, pos_2: i32) {
+// helper function for swaping vec values
+fn swap<T>(arr: &mut Vec<T>, pos_1: i32, pos_2: i32)
+where
+    T: PartialOrd + Copy,
+{
     let temp = arr[pos_1 as usize];
     arr[pos_1 as usize] = arr[pos_2 as usize];
     arr[pos_2 as usize] = temp;
@@ -35,25 +45,25 @@ mod tests {
     use super::*;
     #[test]
     fn quicksort_test() {
-        let mut numbers = vec![4, 3, 7, 5, 6, 9, 1, 8];
+        let mut numbers:Vec<i8> = vec![4, 3, 7, 5, 6, 9, 1, 8];
         let last = numbers.len() - 1;
         quicksort(&mut numbers, 0, last as i32);
-        let sorted_numbers: Vec<i32> = vec![1, 3, 4, 5, 6, 7, 8, 9];
+        let sorted_numbers: Vec<i8> = vec![1, 3, 4, 5, 6, 7, 8, 9];
         assert_eq!(sorted_numbers, numbers);
     }
     #[test]
     fn swap_test() {
-        let mut numbers = vec![4, 3, 6];
+        let mut numbers:Vec<i8>  = vec![4, 3, 6];
         swap(&mut numbers, 0, 2);
-        let swaped_numbers = vec![6, 3, 4];
+        let swaped_numbers:Vec<i8>  = vec![6, 3, 4];
         assert_eq!(swaped_numbers, numbers);
     }
     #[test]
     fn partition_test() {
-        let mut numbers = vec![4, 2, 6, 3, 8, 5];
+        let mut numbers:Vec<i8>  = vec![4, 2, 6, 3, 8, 5];
         let last = numbers.len() - 1;
         partition(&mut numbers, 0, last as i32);
-        let partitioned_numbers = vec![4, 2, 3, 5, 8, 6];
+        let partitioned_numbers:Vec<i8>  = vec![4, 2, 3, 5, 8, 6];
         assert_eq!(partitioned_numbers, numbers);
     }
 }
